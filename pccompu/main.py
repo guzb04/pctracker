e8adfbde18e3499c338e14f6c8add07157011acd
guzb04 <guzmanbeyahut@gmail.com>
2025-12-27 21:57:11 -0300

    first commit pccompu

---
+++ pccompu/main.py
@@ -0,0 +1,53 @@
+import time
+import requests
+from bs4 import BeautifulSoup, TemplateString
+
+from bs4 import XMLParsedAsHTMLWarning
+import warnings
+
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
+
+
+def getUrls(urls):
+    urlList = []
+    for url in urls:
+        url = url.text.strip()
+        if url.startswith(PREFIX):
+            parsedText = url[len(PREFIX):]
+            if (parsedText.startswith("memoria-ram") or
+                    parsedText.startswith("tarjetas-de-video)") or
+                    parsedText.startswith("almacenamiento") or
+                    parsedText.startswith("procesadores")):
+                urlList.append(parsedText)
+    return urlList
+
+
+PREFIX = "http://www.pccompu.com.uy/catalogo/componentes/"
+
+r = requests.get("https://www.pccompu.com.uy/sitemap.xml")
+data = r.text
+
+file = open("sitemap.xml", "w")
+file.write(data)
+
+
+with open("sitemap.xml") as siteMap:
+    soup = BeautifulSoup(siteMap, features='lxml')
+
+
+for priority in soup.find_all("priority"):
+    priority.decompose()
+
+urls = soup.find_all("url")
+
+componentList = getUrls(urls)
+
+for component in componentList:
+    componentRequest = requests.get(f"{PREFIX}{component}")
+    time.sleep(1)
+    componentFile = open("tempComponent.html", "w")
+    componentFile.write(componentRequest.text)
+
+    with open("tempComponent.html") as componentHTML:
+        csoup = BeautifulSoup(componentHTML, features='html.parser')
+        print(csoup)
