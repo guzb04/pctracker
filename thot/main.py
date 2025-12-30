4c59e881e7e265837ddb081bee1770b492c711b5
guzb04 <137735140+guzb04@users.noreply.github.com>
2025-12-30 12:55:09 -0300

    changed thot to only do requests by category to reduce api calls

--- thot/main.py
+++ thot/main.py
@@ -16,11 +16,11 @@ i = 1
 495 - DDR5
 """
 
-includedCategories = {27, 28, 29, 38, 39, 23, 24, 495}
+includedCategories = [27, 28, 29, 38, 39, 23, 24, 495]
 
-while True:
+for i in includedCategories:
     r = requests.get(
-        f"https://thotcomputacion.com.uy/wp-json/wp/v2/product?page={i}")
+        f"https://thotcomputacion.com.uy/wp-json/wp/v2/product?product_cat={i}&per_page=100", verify=False)
     data = r.json()
 
     time.sleep(1)
@@ -29,31 +29,29 @@ while True:
         if (len(data) == 3) and data['data']['status'] == 400:
             break
 
-        datalist = []
         for j in data:
-            if {j["product_cat"][0]} <= includedCategories:
-                httpResponse = requests.get(j['link'], stream=True)
-                category = requests.get(
-                    f"https://thotcomputacion.com.uy/wp-json/wp/v2/product_cat/{j['product_cat'][0]}")
-                categoryParsed = category.json()['name']
-                rawData = httpResponse.text
-                file = open("temp.html", "w")
-                file.write(rawData)
-                with open("temp.html") as tempHtml:
-                    soup = BeautifulSoup(tempHtml, features='html.parser')
-                    price = soup.find_all('bdi')
-                    priceString = str(price[0])
-                    newPrice = priceString[62:-6]
+            httpResponse = requests.get(j['link'], stream=True, verify=False)
+            category = requests.get(
+                f"https://thotcomputacion.com.uy/wp-json/wp/v2/product_cat/{j['product_cat'][0]}", verify=False)
+            categoryParsed = category.json()['name']
+            rawData = httpResponse.text
+            file = open("temp.html", "w")
+            file.write(rawData)
+            with open("temp.html") as tempHtml:
+                soup = BeautifulSoup(tempHtml, features='html.parser')
+                price = soup.find_all('bdi')
+                priceString = str(price[0])
+                newPrice = priceString[62:-6]
 
-                dictToExport = {
-                    "title":  j['title']['rendered'],
-                    "price": newPrice,
-                    "category": categoryParsed,
-                    "link": j["link"]
-                }
+            dictToExport = {
+                "title":  j['title']['rendered'],
+                "price": newPrice,
+                "category": categoryParsed,
+                "link": j["link"]
+            }
 
-                jsonReturn = json.dumps(dictToExport)
-                print(jsonReturn, flush=True)
-                time.sleep(1.5)
+            jsonReturn = json.dumps(dictToExport)
+            print(jsonReturn, flush=True)
+            time.sleep(1.5)
 
         i = i+1
